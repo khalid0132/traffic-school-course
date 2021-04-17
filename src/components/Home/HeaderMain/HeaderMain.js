057dd77b20ae770e0567e9b3990b997a3436a431
Khalid Saifullah <khalid0132@gmail.com>
2021-04-17 11:31:53 +0200

    animation done

--- src/components/Home/HeaderMain/HeaderMain.js
+++ src/components/Home/HeaderMain/HeaderMain.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import car from '../../../images/car.jpg';
+import './HeaderMain.css'
 
 
 
@@ -11,8 +12,15 @@ const HeaderMain = () => {
                 <p className="text-secondary mt-5 mb-5">Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis qui in quos! Expedita, vel blanditiis.</p>
             <button className="btn fw-bold text-white text-uppercase" style={{background: "red"}}>Boka Din Tid</button>
             </div>
-            <div className="col-md-6 col-sm-6 col-12">
-                <img src={car} alt="" className="img-fluid rounded"/>
+            <div className="col-md-6 col-sm-6 col-12 item">
+                <div className="front">
+                     <img src={car} alt="" className="img-fluid rounded"/>
+                </div>
+                <div className="back">
+                    <h2 className="text-center m-5">Stockholm Trafik Verket</h2>
+                    <p className="p-5">Inrikesresor och kollektivtrafik
+Enligt smittskyddslagen har var och en i Sverige ansvar att förhindra spridning av smittsamma sjukdomar som covid-19. Här finns information om vad du behöver tänka på vid resor inom landet.</p>
+                </div>
             </div>
         </main>
     );
